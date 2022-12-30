279ba43c12d4bd1f3e1a5f0172b4217b89d48ce9
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-12-30 15:23:25 +0700

    membuat controller dengan fungsi CRUD

---
+++ src/main/java/ws/a/latihanujianjava/BarangController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.a.latihanujianjava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author ASUS
+ */
+@Controller
+@ResponseBody
+public class BarangController {
+    
+    Barang data = new Barang();
+    BarangJpaController actrl = new BarangJpaController();
+    
+    
+    @RequestMapping(value = "/getNamadata/{id}")
+    public String getNama(@PathVariable("id") int id){
+        try{
+            data = actrl.findBarang(id);
+            return data.getNama()+ "<br>" + data.getJumlah();
+        }
+        catch(Exception error){
+            return "Data Barang Tidak Ditemukan";     
+        }
+    }
+
+
+    
+}
---
+++ src/main/java/ws/a/latihanujianjava/javacontroller.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.a.latihanujianjava;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ws.a.latihanujianjava.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author ASUS
+ */
+@Controller
+@ResponseBody
+public class javacontroller {
+    A2020ws data = new A2020ws();
+    A2020wsJpaController actrl = new A2020wsJpaController();
+    private static Map<String, A2020ws> productRepo = new HashMap<>();
+    
+    @RequestMapping("/getdata")
+    public String getData(){
+        data = actrl.findA2020ws(data.getId());
+        
+        return data.getName();
+    }
+    
+    @RequestMapping(value = "/data")
+    public ResponseEntity<Object> getProduct(){
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+    
+    @RequestMapping("/getName/{id}")
+    public String getName(@PathVariable("id") int id){
+        try {
+            data = actrl.findA2020ws(id);
+            return data.getName() + "<br>" + data.getTglLahir();
+        }
+        catch (Exception error){
+            
+            return "Data tidak ada";
+        }
+        
+    }
+    //Delete ById
+    @RequestMapping("/delete/{id}")
+    public String deleteData(@PathVariable("id") int id){
+
+            try{
+                actrl.destroy(id);
+                
+                return id + " deleted";
+            }
+            catch(NonexistentEntityException error){
+                return id + " tidak ada";
+            }
+    }
+}
